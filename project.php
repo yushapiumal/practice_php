e46b77cb7c9c0241d540542a5e959df7f4e22026
yushan piumal <153017083+yushapiumal@users.noreply.github.com>
2023-12-06 22:57:12 -0800

    Add files via upload

---
+++ project.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="Style.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    </head>
+<body>
+
+<form action="welcome_get.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+<div class="div">
+fghdhdhghdfgfdgdfgdfgdfgdfgdgdfg
+</div>
+<h1>yugfgnfdgndf</h1>
+</form>
+
+</body>
+</html>
